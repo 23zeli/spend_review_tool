4cf8c6464c21a5a0a6f761d25742cd31c01b84b8
Mtutuzeli <mtutuzeli@gmail.com>
2026-07-05 13:32:35 +0200

    Created summaryBar component, added interface with props

--- src/components/SummaryBar.tsx
+++ src/components/SummaryBar.tsx
@@ -1,1 +1,21 @@
 const fmt = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
+
+interface Props {
+    count: number;
+    totalCents: number;
+}
+
+export default function SummaryBar({ count, totalCents }: Props) {
+    return(
+        <div>
+            <div>
+                <div>Visible Transaction</div>
+                <div></div>
+            </div>
+            <div>
+                <div>Total Spend</div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
