4f2dfcbaec80a22789db922191ca656e79943316
Mtutuzeli <mtutuzeli@gmail.com>
2026-07-05 13:51:47 +0200

    added cardStyle abd labelStyle

--- src/components/SummaryBar.tsx
+++ src/components/SummaryBar.tsx
@@ -1,3 +1,5 @@
+import type React from "react";
+
 const fmt = new Intl.NumberFormat('en-US', { style: 'currency', currency: 'USD' });
 
 interface Props {
@@ -9,13 +11,25 @@ export default function SummaryBar({ count, totalCents }: Props) {
     return(
         <div style={{display: 'grid', gridTemplateColumns: '1fr 1fr', gap: 8, marginBottom: 14,}}>
             <div>
-                <div>Visible Transaction</div>
-                <div>{count}</div>
+                <div style={cardStyle}>Visible Transaction</div>
+                <div style={labelStyle}>{count}</div>
             </div>
             <div>
-                <div>Total Spend</div>
-                <div>{fmt.format(totalCents/100)}</div>
+                <div style={cardStyle}>Total Spend</div>
+                <div style={labelStyle}>{fmt.format(totalCents/100)}</div>
             </div>
         </div>
     )
+}
+
+const cardStyle: React.CSSProperties = {
+    background: '#F5F5F3',
+    padding: 8,
+    borderRadius: '10px 14px',
+}
+
+const labelStyle: React.CSSProperties = {
+    fontSize: 18,
+    color: '#888',
+    marginBottom: 4,
 }
